3d3f99c89f8bcd625267947e1bbfa8135aecec88
Muhammad Abdiel Firjatullah <abdielfirdie@gmail.com>
2022-08-23 16:27:45 +0700

    init

---
+++ src/scripts/view/view.js
@@ -0,0 +1,31 @@
+import data from "../../DATA.json";
+
+const main = () => {
+  let restaurantElement = "";
+
+  data.restaurants.forEach((resto) => {
+    restaurantElement += `
+      <a class="resto" href="#">
+          <section class="box">
+            <div class="rating">
+              ${
+                resto.rating > 3.0
+                  ? '<i class="fa-solid fa-star"></i>'
+                  : '<i class="fa-solid fa-star-half"></i>'
+              } ${resto.rating}
+            </div>
+            <img src="${resto.pictureId}" alt="${resto.name} Image">
+            <p class="info">${resto.city}</p>
+            <h3 class="title">${resto.name}</h3>
+            <p class="text-overflow">${resto.description}</p>
+        </section>
+      </a>
+    `;
+  });
+
+  document.querySelector(".wrapper").innerHTML = restaurantElement;
+  document.querySelector(".jumbotron").style.backgroundImage =
+    "url('./images/heros/hero-image_4.jpg')";
+};
+
+export default main;
