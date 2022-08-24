742b1c028fddd859fba8b3ad0727a2071aa2b0bb
Muhammad Abdiel Firjatullah <abdielfirdie@gmail.com>
2022-08-24 16:33:54 +0700

    fixed hamburger button, focus

--- src/scripts/view/navigation.js
+++ src/scripts/view/navigation.js
@@ -4,6 +4,13 @@ const mainElement = document.querySelector("main");
 
 hamburgerButtonElement.addEventListener("click", (event) => {
   drawerElement.classList.toggle("open");
+
+  if (drawerElement.classList.contains("open")) {
+    hamburgerButtonElement.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+  } else {
+    hamburgerButtonElement.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+  }
+
   event.stopPropagation();
 });
 
