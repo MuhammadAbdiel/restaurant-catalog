cdafa0f63a5df4d69efdbafb6a97fa81f8f1ebe2
Muhammad Abdiel Firjatullah <abdielfirdie@gmail.com>
2022-09-05 20:05:40 +0700

    update package.json, favorite.js

--- src/scripts/view/pages/favorite.js
+++ src/scripts/view/pages/favorite.js
@@ -5,12 +5,14 @@ import '../../component/restaurant-list';
 class Favorite {
   static async render() {
     return `<h1>Your Favorite Restaurant</h1>
+        <h2 id="emptyData">There are no favorite restaurants yet</h2>
         <restaurant-list class="wrapper" id="content"></restaurant-list>`;
   }
 
   static async afterRender() {
     const restaurantListElement = document.querySelector('restaurant-list');
     const favoriteData = await LocalData.getAllSaved();
+    const emptyCaption = document.querySelector('#emptyData');
 
     if (favoriteData.length == 0) {
       Swal.fire({
@@ -18,6 +20,10 @@ class Favorite {
         text: "There are no favorite restaurants yet!",
         icon: "warning",
       });
+    }
+    
+    if (favoriteData.length > 0){
+      emptyCaption.style.display = 'none';
     }
 
     restaurantListElement.restaurants = favoriteData;
