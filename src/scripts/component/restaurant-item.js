8252c4af199a2631db4ec56c537abee69bc854f6
Muhammad Abdiel Firjatullah <abdielfirdie@gmail.com>
2022-09-10 19:59:12 +0700

    update lazyload image, image responsive

--- src/scripts/component/restaurant-item.js
+++ src/scripts/component/restaurant-item.js
@@ -1,6 +1,6 @@
 import CONFIG from '../data/config';
 import 'lazysizes';
-import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit.js';
 
 class RestaurantItem extends HTMLElement {
   set restaurantItem(restaurant) {
@@ -19,9 +19,8 @@ class RestaurantItem extends HTMLElement {
                   : '<i class="fa-solid fa-star-half"></i>'
               } ${this.restaurant.rating}
             </div>
-            <img src="${CONFIG.BASE_IMAGE_URL}${
-      this.restaurant.pictureId
-    }" alt="${this.restaurant.name} Image">
+            <img class="lazyload" data-src="${CONFIG.BASE_IMAGE_URL}${this.restaurant.pictureId}" src="${CONFIG.BASE_IMAGE_URL}${
+      this.restaurant.pictureId}" alt="${this.restaurant.name} Image">
             <p class="info">${this.restaurant.city}</p>
             <h3 class="title">${this.restaurant.name}</h3>
             <p class="text-overflow">${this.restaurant.description}</p>
--- src/scripts/view/pages/home.js
+++ src/scripts/view/pages/home.js
@@ -7,6 +7,12 @@ class Home {
   static async render() {
     return `
       <section class="jumbotron">
+          <picture>
+            <source media="(min-width:601px)" srcset="./images/heros/hero-image_2-large.jpg">
+            <source media="(max-width:600px)" srcset="./images/heros/hero-image_2-small.jpg">
+            <img src="./images/heros/hero-image_2-large.jpg" alt="Heroes Image">
+          </picture>
+
           <h1 class="jumbotron-title">Welcome to RESTFood!</h1>
       </section>
       
@@ -19,12 +25,12 @@ class Home {
   }
 
   static async afterRender() {
-    const jumbotronElement = document.querySelector(".jumbotron");
+    // const jumbotronElement = document.querySelector(".jumbotron");
     const loadingElement = document.querySelector('loading-indicator');
     const restaurantListElement = document.querySelector('restaurant-list');
 
-    jumbotronElement.style.backgroundImage =
-      "url('./images/heros/hero-image_4.jpg')";
+    // jumbotronElement.style.backgroundImage =
+    //   "url('./images/heros/hero-image_2-large.jpg')";
 
     try {
       const response = await ApiRepository.getRestaurantList();
