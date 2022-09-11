510f51fdd4b65db8dc74f9ff11cb3d7845424569
Muhammad Abdiel Firjatullah <abdielfirdie@gmail.com>
2022-09-11 17:19:24 +0700

    update .eslintr.json

--- src/scripts/component/restaurant-item.js
+++ src/scripts/component/restaurant-item.js
@@ -19,8 +19,7 @@ class RestaurantItem extends HTMLElement {
                   : '<i class="fa-solid fa-star-half"></i>'
               } ${this.restaurant.rating}
             </div>
-            <img class="lazyload" data-src="${CONFIG.BASE_IMAGE_URL}${this.restaurant.pictureId}" src="${CONFIG.BASE_IMAGE_URL}${
-      this.restaurant.pictureId}" alt="${this.restaurant.name} Image">
+            <img class="lazyload" data-src="${CONFIG.BASE_IMAGE_URL}${this.restaurant.pictureId}" alt="${this.restaurant.name} Image">
             <p class="info">${this.restaurant.city}</p>
             <h3 class="title">${this.restaurant.name}</h3>
             <p class="text-overflow">${this.restaurant.description}</p>
