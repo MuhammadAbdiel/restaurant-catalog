a4ca0c76f1aa27c6a5b5a1e4125aafa01269d34a
Muhammad Abdiel Firjatullah <abdielfirdie@gmail.com>
2022-09-12 15:32:05 +0700

    update

--- src/scripts/component/app-bar.js
+++ src/scripts/component/app-bar.js
@@ -15,17 +15,17 @@ class TabLayout extends HTMLElement {
   render() {
     this.innerHTML = `
       <a href="../#content" class="skip-link">Skip to Content</a>
-      <div class="app-bar__menu">
-        <button id="hamburger" type="button"><i class="fa-solid fa-bars"></i></button>
-      </div>
-      <nav id="drawer">
-          <a href="index.html" class="drawer-title">RESTFood</a>
-          <ul>
-              <li><a href="#/">Home</a></li>
-              <li><a href="#/favorite">Favorite</a></li>
-              <li><a href="https://github.com/MuhammadAbdiel" target="_blank" rel="noopener">About Us</a></li>
-          </ul>
-      </nav>
+        <div class="app-bar__menu">
+          <button id="hamburger" type="button"><i class="fa-solid fa-bars"></i></button>
+        </div>
+        <nav id="drawer">
+            <a href="index.html" class="drawer-title">RESTFood</a>
+            <ul>
+                <li><a href="#/">Home</a></li>
+                <li><a href="#/favorite">Favorite</a></li>
+                <li><a href="https://github.com/MuhammadAbdiel" target="_blank" rel="noopener">About Us</a></li>
+            </ul>
+        </nav>
       <a href="index.html" class="main-title">RESTFood</a>
     `;
   }
--- src/scripts/component/restaurant-item.js
+++ src/scripts/component/restaurant-item.js
@@ -11,18 +11,18 @@ class RestaurantItem extends HTMLElement {
   render() {
     this.innerHTML = `
       <a class="resto" href="#/detail/${this.restaurant.id}">
-          <section class="box">
-            <div class="rating">
-              ${
-                this.restaurant.rating >= 2.5
-                  ? '<i class="fa-solid fa-star"></i>'
-                  : '<i class="fa-solid fa-star-half"></i>'
-              } ${this.restaurant.rating}
-            </div>
-            <img class="lazyload" data-src="${CONFIG.BASE_IMAGE_URL}${this.restaurant.pictureId}" alt="${this.restaurant.name} Image">
-            <p class="info">${this.restaurant.city}</p>
-            <h3 class="title">${this.restaurant.name}</h3>
-            <p class="text-overflow">${this.restaurant.description}</p>
+        <section class="box">
+          <div class="rating">
+            ${
+              this.restaurant.rating >= 2.5
+                ? '<i class="fa-solid fa-star"></i>'
+                : '<i class="fa-solid fa-star-half"></i>'
+            } ${this.restaurant.rating}
+          </div>
+          <img class="lazyload" data-src="${CONFIG.BASE_IMAGE_URL}${this.restaurant.pictureId}" alt="${this.restaurant.name} Image">
+          <p class="info">${this.restaurant.city}</p>
+          <h3 class="title">${this.restaurant.name}</h3>
+          <p class="text-overflow">${this.restaurant.description}</p>
         </section>
       </a>
     `;
